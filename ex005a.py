1a10db6544901ff5ffe61821fcd3af46e5743397
Luciana de Carvalho Corrêa <lucianadcorrea@gmail.com>
2026-03-19 16:19:20 -0300

    Update ex005a.py

--- ex005a.py
+++ ex005a.py
@@ -8,4 +8,4 @@ di=n1/n2
 pot=n1**n2
 dit=n1//n2
 rest=n1%n2
-print('soma {},\nsubstração {},\nmultuplicação {},\ndivisão {:.2f},\npotenciação {},\ndivisão inteira {},\nresto da divisão {}'.format(s,sub,mult,di, pot, dit, rest))
+print('soma {},\nsubstração {},\nmultuplicação {},\ndivisão {:.2f},\npotenciação {},\ndivisão inteira {},\nresto da divisão {}'.format(s, sub, mult, di, pot, dit, rest))
