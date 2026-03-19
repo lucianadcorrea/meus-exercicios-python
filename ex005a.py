99d6e344e18575decd7f9db0aafae91c73fad6b3
Luciana de Carvalho Corrêa <lucianadcorrea@gmail.com>
2026-03-19 16:15:11 -0300

    Update ex005a.py

--- ex005a.py
+++ ex005a.py
@@ -1,3 +1,4 @@
+#Crie um programa que digite dois valores e faça a soma, subtração,multiplicação,divisão,potenciação,divisão inteira e resto desses dois números.
 n1=int(input('Digite um valor: '))
 n2=int(input('Digite outro valor: '))
 s=n1+n2
