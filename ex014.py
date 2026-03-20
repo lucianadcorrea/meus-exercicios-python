f3ced37cd28d5d7a33be6018cb867f94793f8512
Luciana de Carvalho Corrêa <lucianadcorrea@gmail.com>
2026-03-19 22:21:58 -0300

    Add files via upload

---
+++ ex014.py
@@ -0,0 +1,7 @@
+'''
+ Escreva um programa que converta uma temperatura digitando em graus Celsius
+ e converta para graus Fahrenheit.
+'''
+c=float(input('Qual a temperatura atual em graus °C?'))
+f=9*c/5+32
+print('A temperatura {}C° em Fahrenheit é {}°F'.format(c, f))
